07208c91007447c23637958dc9469badd2f1d1b1
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-20 00:44:44 +0400

    fix some problems

--- routes/users/index.js
+++ routes/users/index.js
@@ -40,12 +40,10 @@ router.route("/follow/channel").post(async (req, res) => {
   try {
     const { channelId, userId } = req.body;
 
-    await UserSchema.findById(userId).then((result) => {
-      const data = result.followedChannel;
-      data.push(channelId);
-
-      result.followedChannel = data;
-      result.save((err, doc) => {
+    UserSchema.findByIdAndUpdate(
+      { _id: userId },
+      { $push: { followedChannel: channelId } },
+      (err) => {
         if (!err) {
           res.json({
             success: true,
@@ -54,8 +52,8 @@ router.route("/follow/channel").post(async (req, res) => {
         } else {
           res.json({ success: false, msg: "Sorry, Something is wrong." });
         }
-      });
-    });
+      }
+    );
   } catch (err) {
     res.status(502).json(err);
   }
