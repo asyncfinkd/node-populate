d4c9b5936089104768e7e0c2f5544aadb020703b
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-20 00:01:49 +0400

    start populating

--- routes/users/index.js
+++ routes/users/index.js
@@ -2,13 +2,23 @@ const router = require("express").Router();
 const UserSchema = require("../../models/users/index.model");
 const ChannelSchema = require("../../models/channels/index.model");
 
-router.route("/get/users").get(async (req, res) => {});
+router.route("/get/users").get(async (req, res) => {
+  try {
+    await UserSchema.find()
+      .populate("followedChannel")
+      .then((result) => {
+        res.json(result);
+      });
+  } catch (err) {
+    res.json(err);
+  }
+});
 
 router.route("/add/user").post(async (req, res) => {
   try {
     const newUser = new UserSchema(req.body);
 
-    newUser.save().then(() => {
+    await newUser.save().then(() => {
       res.json({
         success: true,
         msg: "Congratulation, User is added successfuly.!",
@@ -23,14 +33,32 @@ router.route("/follow/channel").post(async (req, res) => {
   try {
     const { channelId, userId } = req.body;
 
-    await UserSchema.findById(userId).then((res) => {
-      followChannel.save((err, doc) => {
+    await UserSchema.findById(userId).then((result) => {
+      const data = result.followedChannel;
+      data.push(channelId);
+
+      result.followedChannel = data;
+      result.save((err, doc) => {
         if (!err) {
-          res.json({ success: true, msg: "" });
+          res.json({
+            success: true,
+            msg: "Congratulation, You Followed Company",
+          });
         } else {
           res.json({ success: false, msg: "Sorry, Something is wrong." });
         }
       });
+
+      // addToFollow.save((err, doc) => {
+      //   if (!err) {
+      //     res.json({
+      //       success: true,
+      //       msg: "Congratulation, You Followed Company",
+      //     });
+      //   } else {
+      //     res.json({ success: false, msg: "Sorry, Something is wrong." });
+      //   }
+      // });
     });
   } catch (err) {
     res.status(502).json(err);
